aa9aea921d16df7ad9ca2a77fb3fff9a9d689e3c
Cpapot <corentinpapot2004@gmail.com>
2024-06-23 16:42:56 +0200

    try to open openGL window & open file explorator

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-struct Structure {
+/*struct Structure {
 	field1:i32,
 	field2:i32,
 	field3:i32
@@ -31,4 +31,71 @@ fn main()
 	println!("value {:?}", stru.field1);
 
 	panic!("This is a panic message");
+}*/
+
+/*extern crate glutin;
+extern crate gl;
+
+use glutin::dpi::LogicalSize;
+use glutin::event::{Event, WindowEvent};
+use glutin::event_loop::ControlFlow;
+use glutin::window::WindowBuilder;
+use glutin::ContextBuilder;
+
+fn main() {
+    // Création de l'événement et de la fenêtre
+    let event_loop = glutin::event_loop::EventLoop::new();
+    let wb = WindowBuilder::new()
+        .with_title("Fenêtre OpenGL")
+        .with_inner_size(LogicalSize::new(800.0, 600.0));
+
+    let windowed_context = ContextBuilder::new()
+        .with_vsync(true)
+        .build_windowed(wb, &event_loop)
+        .unwrap();
+
+    // Rendre le contexte OpenGL actuel
+    let windowed_context = unsafe { windowed_context.make_current().unwrap() };
+
+    // Charger les fonctions OpenGL
+    gl::load_with(|symbol| windowed_context.get_proc_address(symbol) as *const _);
+
+    // Configuration OpenGL
+    unsafe {
+        gl::ClearColor(0.1, 0.2, 0.3, 1.0);
+    }
+
+    // Boucle d'événements
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                _ => (),
+            },
+            Event::RedrawRequested(_) => {
+                // Rendu OpenGL
+                unsafe {
+                    gl::Clear(gl::COLOR_BUFFER_BIT);
+                }
+                windowed_context.swap_buffers().unwrap();
+            },
+            _ => (),
+        }
+    });
+}*/
+
+extern crate rfd;
+
+fn main() {
+    // Ouvrir un explorateur de fichiers pour choisir un fichier
+    let file = rfd::FileDialog::new()
+        .set_title("Choisir un fichier")
+        .pick_file();
+
+    match file {
+        Some(path) => println!("Fichier sélectionné : {:?}", path),
+        None => println!("Aucun fichier sélectionné"),
+    }
 }
