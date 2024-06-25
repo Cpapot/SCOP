fd9c42527dbbb13620ffc058f153b4c471c5c947
Cpapot <corentinpapot2004@gmail.com>
2024-06-25 22:51:50 +0200

    check for invalid value

--- src/parsing.rs
+++ src/parsing.rs
@@ -1,8 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.rs                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cpapot <cpapot@student.42lyon.fr >         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/25 22:48:41 by cpapot            #+#    #+#             */
+/*   Updated: 2024/06/25 22:49:19 by cpapot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 pub struct Objdata {
 	vertex:Vec<(f64, f64, f64)>,
 	normal:Vec<(f64, f64, f64)>,
 	face:Vec<(f64, f64, f64)>
  }
+
+macro_rules! get_value {
+	($lineindex:expr, $words:expr) => {
+		$words.next().ok_or_else(|| std::io::Error::new(std::io::ErrorKind::InvalidInput, format!("invalid value at line: {}", $lineindex)))?
+		.parse::<f64>().map_err(|_| std::io::Error::new(std::io::ErrorKind::InvalidInput, format!("invalid value at line: {}", $lineindex)))?
+	};
+}
 
 pub fn print_objdata(data : &Objdata)
 {
@@ -31,18 +50,20 @@ fn fill_struct(content: &String) -> Result<Objdata, std::io::Error>
 {
 	let mut data = Objdata{vertex:Vec::new(), normal:Vec::new(), face:Vec::new()};
 	let lines = content.lines();
+	let mut lineindex = 0;
 	for line in lines
 	{
+		lineindex += 1;
 		let mut words = line.split_whitespace();
 		let word = words.next().unwrap_or("None");
 		if word == "None" || word == "#" {continue;}
 		if word == "v"
 		{
-			let size = words.clone().count();
+			let size: usize = words.clone().count();
 			if size != 3 {return Err(std::io::Error::new(std::io::ErrorKind::InvalidInput, "vertex is not a 3D point"));}
-			let x = words.next().unwrap().parse::<f64>().unwrap();
-			let y = words.next().unwrap().parse::<f64>().unwrap();
-			let z = words.next().unwrap().parse::<f64>().unwrap();
+			let x = get_value!(lineindex, words);
+			let y = get_value!(lineindex, words);
+			let z = get_value!(lineindex, words);
 			data.vertex.push((x, y, z));
 		}
 		if word == "f"
@@ -51,9 +72,9 @@ fn fill_struct(content: &String) -> Result<Objdata, std::io::Error>
 			let mut index = 3;
 			if size < 3 {return Err(std::io::Error::new(std::io::ErrorKind::InvalidInput, "face is not a 3D point"));}
 
-			let x = words.next().unwrap().parse::<f64>().unwrap();
-			let y = words.next().unwrap().parse::<f64>().unwrap();
-			let z = words.next().unwrap().parse::<f64>().unwrap();
+			let x = get_value!(lineindex, words);
+			let y = get_value!(lineindex, words);
+			let z = get_value!(lineindex, words);
 			data.face.push((x, y, z));
 			while index < size
 			{
