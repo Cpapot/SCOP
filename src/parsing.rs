d98d6f6c63e577ff164d517f209298e5f89aa04c
Cpapot <cpapot@student.42lyon.fr>
2024-07-22 14:57:36 +0200

    true 3d

--- src/parsing.rs
+++ src/parsing.rs
@@ -6,21 +6,46 @@
 /*   By: cpapot <cpapot@student.42lyon.fr >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 22:48:41 by cpapot            #+#    #+#             */
-/*   Updated: 2024/07/19 15:27:55 by cpapot           ###   ########.fr       */
+/*   Updated: 2024/07/22 14:50:20 by cpapot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 pub struct Objdata {
 	pub vertex:Vec<(f64, f64, f64)>,
-	normal:Vec<(f64, f64, f64)>,
+	pub normal:Vec<(f64, f64, f64)>,
 	pub indexs:Vec<u16>,
-	pub face:Vec<(f64, f64, f64)>
  }
+
+ fn calculate_normal(v1: (f64, f64, f64), v2: (f64, f64, f64), v3: (f64, f64, f64)) -> (f64, f64, f64) {
+    let u = (
+        v2.0 - v1.0,
+        v2.1 - v1.1,
+        v2.2 - v1.2,
+    );
+    let v = (
+        v3.0 - v1.0,
+        v3.1 - v1.1,
+        v3.2 - v1.2,
+    );
+
+    let normal = (
+        u.1 * v.2 - u.2 * v.1,
+        u.2 * v.0 - u.0 * v.2,
+        u.0 * v.1 - u.1 * v.0,
+    );
+
+    let length = (normal.0 * normal.0 + normal.1 * normal.1 + normal.2 * normal.2).sqrt();
+    (
+        normal.0 / length,
+        normal.1 / length,
+        normal.2 / length,
+    )
+}
 
  macro_rules! get_u16_value {
 	($lineindex:expr, $words:expr) => {
 		$words.next().ok_or_else(|| std::io::Error::new(std::io::ErrorKind::InvalidInput, format!("invalid value at line: {}", $lineindex)))?
-		.parse::<u16>().map_err(|_| std::io::Error::new(std::io::ErrorKind::InvalidInput, format!("invalid value at line: {}", $lineindex)))?
+		.parse::<u32>().map_err(|_| std::io::Error::new(std::io::ErrorKind::InvalidInput, format!("invalid value at line: {}", $lineindex)))?
 	};
 }
 
@@ -37,10 +62,10 @@ pub fn print_objdata(data : &Objdata)
 	{
 		println!("vertex: x:{:?}, y:{:?}, z:{:?}", data.vertex[i].0, data.vertex[i].1, data.vertex[i].2);
 	}
-	for i in 0..data.face.len()
+	/*for i in 0..data.face.len()
 	{
 		println!("face: x:{:?}, y:{:?}, z:{:?}", data.face[i].0, data.face[i].1, data.face[i].2);
-	}
+	}*/
 }
 
 fn	read_file(path: &str) -> Result<String, std::io::Error>
@@ -56,9 +81,10 @@ fn	read_file(path: &str) -> Result<String, std::io::Error>
 
 fn fill_struct(content: &String) -> Result<Objdata, std::io::Error>
 {
-	let mut data = Objdata{vertex:Vec::new(), normal:Vec::new(), face:Vec::new(), indexs:Vec::new()};
+	let mut data = Objdata{vertex:Vec::new(), normal:Vec::new(), indexs:Vec::new()};
 	let lines = content.lines();
 	let mut lineindex = 0;
+	let mut isnormal: i32 = 0;
 	for line in lines
 	{
 		lineindex += 1;
@@ -82,15 +108,29 @@ fn fill_struct(content: &String) -> Result<Objdata, std::io::Error>
 
 			while index < size
 			{
-				let value: u16 = get_u16_value!(lineindex, words);
-				data.indexs.push(value - 1);
+				let value: u32 = get_u16_value!(lineindex, words);
+				data.indexs.push(value as u16 - 1);
 				index += 1;
 			}
+		}
+		if word == "vn"
+		{
+			isnormal = 1;
+		}
+	}
+	if isnormal == 0
+	{
+		for face in data.indexs.chunks(3)
+		{
+			let v1 = data.vertex[face[0] as usize];
+			let v2 = data.vertex[face[1] as usize];
+			let v3 = data.vertex[face[2] as usize];
+			let normal = calculate_normal(v1, v2, v3);
+			data.normal.push(normal);
 		}
 	}
 	return Ok(data);
 }
-
 
 pub fn parse_obj(path: &str) -> Result<Objdata, std::io::Error>
 {
